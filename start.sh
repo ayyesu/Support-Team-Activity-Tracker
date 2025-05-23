a8a5074e19cf89139509540abff049911ee2f1c3
ayyesu <iamdanielkissiedu@gmail.com>
2025-05-23 05:28:14 +0000

    port fix

--- start.sh
+++ start.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-envsubst < /etc/nginx/conf.d/default.template > /etc/nginx/conf.d/default.conf
+export PORT=${PORT:-80}
+envsubst '$PORT' < /etc/nginx/conf.d/default.template > /etc/nginx/conf.d/default.conf
 # Start Nginx and PHP-FPM
 nginx
+php artisan config:cache
+php artisan migrate:fresh --force --seed
 php-fpm
 tail -f /dev/null
